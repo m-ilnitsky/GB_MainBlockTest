011642a27acc2f904bc52557c2453db0e2aeef13
Mike_Prim-5 <mike-nsk@ngs.ru>
2023-07-17 15:21:05 +0700

    Added reading from console

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Linq;
 
-string[] GetStringsWithMaxLength(string[] strings, int maxLength)
+int ReadInt(string message, int minNumber = int.MinValue, int maxNumber = int.MaxValue)
+{
+    Console.Write(message);
+    string line = Console.ReadLine();
+
+    while (true)
+    {
+        if (int.TryParse(line, out int number))
+        {
+            if (minNumber <= number && number <= maxNumber)
+            {
+                return number;
+            }
+
+            Console.WriteLine($"Введите число в диапазоне от {minNumber} до {maxNumber}!");
+            continue;
+        }
+
+        Console.WriteLine($"Введите коррекное целое число!");
+    }
+}
+
+string ReadString(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? "";
+}
+
+string[] ReadStringsArray()
+{
+    int stringsCount = ReadInt("Введите количество строк массива: ", 0, 10);
+    string[] strings = new string[stringsCount];
+
+    for (int i = 0; i < stringsCount; i++)
+    {
+        strings[i] = ReadString($"Введите {i + 1}-ю строку массива: ");
+    }
+
+    return strings;
+}
+
+string[] GetStringsWithMaxLength(string[] strings, int maxStringLength)
 {
     int resultStringsCount = 0;
 
     foreach (string str in strings)
     {
-        if (str.Length <= maxLength)
+        if (str.Length <= maxStringLength)
         {
             resultStringsCount++;
         }
@@ -17,7 +58,7 @@ string[] GetStringsWithMaxLength(string[] strings, int maxLength)
 
     for (int i = 0, j = 0; i < strings.Length && j < resultStrings.Length; i++)
     {
-        if (strings[i].Length <= maxLength)
+        if (strings[i].Length <= maxStringLength)
         {
             resultStrings[j] = strings[i];
             j++;
@@ -32,12 +73,17 @@ void WriteStringsArray(string[] strings)
     Console.Write($"[{string.Join(", ", strings.Select(s => $"\"{s}\""))}]");
 }
 
-void WriteArrays(string[] sourceArray, string[] resultArray)
+void WriteStringsArrays(string[] sourceArray, string[] resultArray)
 {
     WriteStringsArray(sourceArray);
     Console.Write(" => ");
     WriteStringsArray(resultArray);
     Console.WriteLine();
+}
+
+void ConvertAndWrite(string[] strings, int maxStringLength)
+{
+    WriteStringsArrays(strings, GetStringsWithMaxLength(strings, maxStringLength));
 }
 
 const int maxStringLength = 3;
@@ -46,11 +92,11 @@ string[] testStrings1 = { "Hello", "2", "world", ":-)" };
 string[] testStrings2 = { "1234", "1567", "-2", "computer science" };
 string[] testStrings3 = { "Russia", "Denmark", "Kazan" };
 
-string[] resultStrings1 = GetStringsWithMaxLength(testStrings1, maxStringLength);
-string[] resultStrings2 = GetStringsWithMaxLength(testStrings2, maxStringLength);
-string[] resultStrings3 = GetStringsWithMaxLength(testStrings3, maxStringLength);
+string[] userStrings = ReadStringsArray();
 
-WriteArrays(testStrings1, resultStrings1);
-WriteArrays(testStrings2, resultStrings2);
-WriteArrays(testStrings3, resultStrings3);
+Console.WriteLine();
+ConvertAndWrite(testStrings1, maxStringLength);
+ConvertAndWrite(testStrings2, maxStringLength);
+ConvertAndWrite(testStrings3, maxStringLength);
+ConvertAndWrite(userStrings, maxStringLength);
 Console.WriteLine();
